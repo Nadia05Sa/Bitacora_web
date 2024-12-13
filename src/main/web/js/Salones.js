67af16e72886d75c4629460f8efa2fefa5c56a1f
GleipM <20233tn181@utez.edu.mx>
2024-12-13 02:50:39 -0600

    prueba diseño

--- src/main/web/js/Salones.js
+++ src/main/web/js/Salones.js
@@ -96,27 +96,11 @@ function cargarSalones() {
                     div.innerHTML = `
                         <style>
                              body {
-                                margin: 0;
-                                padding: 0;
-                                display: flex;
-                                flex-direction: column;
-                                justify-content: flex-start;
-                                align-items: center;
                                 height: 100%;
-                                background: no-repeat center/cover url("../img/salonConPersonas.jpg");
-                                font-family: 'Montserrat Alternates', sans-serif;
-                                position: relative;
-                                color: #333;
                             }
                             
                             .overlay {
-                                position: absolute;
-                                top: 0;
-                                left: 0;
-                                width: 100%;
                                 height: 100%;
-                                background-color: rgba(0, 0, 0, 0.5);
-                                z-index: 1;
                             }
                         </style>
                         <div>
